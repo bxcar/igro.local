77a653d97eb3f4b3521eb5dec6848dbb756653d3
bxcar <malanchukdima@mail.ru>
2018-04-15 17:14:08 +0300

    single.html responsive and fix bug owl mobile scroll on about.html

--- dist/js/common.js
+++ dist/js/common.js
@@ -6,7 +6,17 @@ $(document).ready(function () {
         dotsContainer: '#carousel-custom-dots'
     });
 
+    $("#owl-slider-v2").owlCarousel({
+        items: 1,
+        singleItem: true,
+        dots: true,
+        dotsContainer: '#carousel-custom-dots',
+        animateOut: 'fadeOut',
+        autoHeight:true
+    });
+
     $('.owl-dot').click(function () {
         $("#owl-slider").trigger('to.owl.carousel', [$(this).index(), 300]);
+        $("#owl-slider-v2").trigger('to.owl.carousel', [$(this).index(), 300]);
     });
 });
